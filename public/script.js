666b9129c849d6b2395608be52a966a675d0293d
verycosy <red_horse_@naver.com>
2020-01-16 03:09:09 +0900

    Click/Drop To Select Files

--- public/script.js
+++ public/script.js
@@ -1,1 +1,35 @@
 const socket = io();
+
+const fileList = document.getElementById("fileList");
+
+//FIXME: Directoy Not Supported Yet
+function handleFileSelect(evt) {
+  evt.preventDefault();
+
+  const files = evt.target.files || evt.dataTransfer.files;
+  addFilesToList(files);
+}
+
+function addFilesToList(files) {
+  fileList.innerHTML = "";
+
+  for (let i = 0; i < files.length; i++) {
+    const li = document.createElement("li");
+    li.textContent = files[i].name;
+    fileList.appendChild(li);
+  }
+}
+
+function handleFileDrag(evt) {
+  evt.preventDefault();
+
+  evt.dataTransfer.dropEffect = "copy";
+}
+
+(function init() {
+  const dropbox = document.getElementById("dropbox");
+  dropbox.addEventListener("dragover", handleFileDrag);
+  dropbox.addEventListener("drop", handleFileSelect);
+
+  document.getElementById("files").addEventListener("change", handleFileSelect);
+})();
